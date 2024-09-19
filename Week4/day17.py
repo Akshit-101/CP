b19f679f0796ca47e58c4a14bd0cf7964087dbbb
aki <akshitpjt07@gmail.com>
2024-09-19 09:24:01 +0530

    day17

---
+++ Week4/day17.py
@@ -0,0 +1,70 @@
+
+# ------------------------------------- Find the Smallest Divisor Given a Threshold -------------------------------------
+
+import math
+
+def sumByD(arr, div):
+    n = len(arr) 
+    total_sum = 0
+    for i in range(n):
+        total_sum += math.ceil(arr[i] / div)
+    return total_sum
+
+def smallestDivisor(arr, limit):
+    n = len(arr)
+    if n > limit:
+        return -1
+    low = 1
+    high = max(arr)
+
+    # Apply binary search
+    while low <= high:
+        mid = (low + high) // 2
+        if sumByD(arr, mid) <= limit:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return low
+
+arr = [1, 2, 3, 4, 5]
+limit = 8
+ans = smallestDivisor(arr, limit)
+print("The minimum divisor is:", ans)
+
+
+# ---------------------------------- Aggressive Cows  ----------------------------------
+
+
+def canWePlace(stalls, dist, cows):
+    n = len(stalls) 
+    cntCows = 1  
+    last = stalls[0]  # position of last placed cow
+    for i in range(1, n):
+        if stalls[i] - last >= dist:
+            cntCows += 1  # place next cow
+            last = stalls[i]  # update the last location
+        if cntCows >= cows:
+            return True
+    return False
+
+def aggressiveCows(stalls, k):
+    n = len(stalls)
+    stalls.sort() 
+
+    low = 1
+    high = stalls[n - 1] - stalls[0]
+    # apply binary search
+    while low <= high:
+        mid = (low + high) // 2
+        if canWePlace(stalls, mid, k):
+            low = mid + 1
+        else:
+            high = mid - 1
+    return high
+
+stalls = [0, 3, 4, 7, 10, 9]
+k = 4
+ans = aggressiveCows(stalls, k)
+print("The maximum possible minimum distance is:", ans)
+
+
